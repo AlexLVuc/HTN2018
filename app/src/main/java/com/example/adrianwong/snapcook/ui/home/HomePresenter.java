7573ca83093980cc6e2acbf25d7f582bb2cc85eb
Aaditya Sondhi <aaditya-mac@v1020-wn-195-209.campus-dynamic.uwaterloo.ca>
2018-09-15 14:41:04 -0400

    Added functionality to home screen button

--- app/src/main/java/com/example/adrianwong/snapcook/ui/home/HomePresenter.java
+++ app/src/main/java/com/example/adrianwong/snapcook/ui/home/HomePresenter.java
@@ -1,7 +1,13 @@
 package com.example.adrianwong.snapcook.ui.home;
 
-public class HomePresenter {
+import com.example.adrianwong.snapcook.ui.base.BasePresenter;
 
+public class HomePresenter extends BasePresenter<HomeActivity> {
 
+    public HomePresenter() {
+    }
 
+    public void onButtonClick() {
+        mView.startCameraActivity();
+    }
 }
