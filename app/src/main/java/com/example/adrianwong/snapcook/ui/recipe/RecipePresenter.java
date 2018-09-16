f9eca5eca049c334230465336ce6915dcb842571
a6sondhi <a6sondhi@gmail.com>
2018-09-16 01:34:53 -0400

    WIP

--- app/src/main/java/com/example/adrianwong/snapcook/ui/recipe/RecipePresenter.java
+++ app/src/main/java/com/example/adrianwong/snapcook/ui/recipe/RecipePresenter.java
@@ -1,14 +1,52 @@
 package com.example.adrianwong.snapcook.ui.recipe;
 
+import com.example.adrianwong.snapcook.adapter.RecipeAdapter;
+import com.example.adrianwong.snapcook.model.Recipe;
+import com.example.adrianwong.snapcook.network.SnapcookRestAdapter;
 import com.example.adrianwong.snapcook.ui.base.BasePresenter;
+
+import java.util.List;
 
 import javax.inject.Inject;
 
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class RecipePresenter extends BasePresenter<RecipeActivity> {
 
-    @Inject
-    public RecipePresenter() {
+    private RecipeAdapter recipeAdapter;
+    private SnapcookRestAdapter snapcookRestAdapter;
+    private CompositeDisposable disposable;
 
+    @Inject
+    public RecipePresenter(SnapcookRestAdapter snapcookRestAdapter, CompositeDisposable disposable, RecipeAdapter recipeAdapter) {
+        this.snapcookRestAdapter = snapcookRestAdapter;
+        this.disposable = disposable;
+        this.recipeAdapter = recipeAdapter;
+    }
+
+    public void getRecipeList(String key, String accept, String ingredients, int number, int ranking) {
+        disposable.add(snapcookRestAdapter.getRecipeList(key, accept, ingredients, number, ranking)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<List<Recipe>>() {
+                    @Override
+                    public void onNext(List<Recipe> recipes) {
+                        recipeAdapter.set
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                }));
     }
 
 }
